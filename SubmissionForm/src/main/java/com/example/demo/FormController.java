417d0e5380eb752b170fad835240cd58bac0a91c
mddmustainbillah <19103007@iubat.edu>
2022-02-27 17:21:06 +0600

    api

--- SubmissionForm/src/main/java/com/example/demo/FormController.java
+++ SubmissionForm/src/main/java/com/example/demo/FormController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,6 +68,21 @@ public class FormController {
 	
 	@PostMapping("/customers")
 	public Customers getCustomers3(@RequestBody Customers customers)
+	{
+		repo.save(customers);
+		return customers;
+	}
+	
+	@DeleteMapping("/customers/{cid}")
+	public Customers getCustomers4(@PathVariable("cid") int cid)
+	{
+		Customers cust = repo.getOne(cid);
+		repo.delete(cust);
+		return cust;
+	}
+	
+	@PutMapping(path="/customers", consumes= {"application/json"})
+	public Customers getCustomers5(@RequestBody Customers customers)
 	{
 		repo.save(customers);
 		return customers;
